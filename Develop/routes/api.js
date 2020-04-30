5c989ee47a7b63266c0677cf2b61f6f21d17d783
ianm93 <45110918+iannm93@users.noreply.github.com>
2020-04-29 18:34:23 -0700

    reconfigured schema

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,18 +1,40 @@
 const router = require("express").Router();
-
+// const db = require("../models/")
 // TODO: import required model/s
-
 
 // TODO: and add code to the routes so that the app functions correctly
 
-router.post("/api/workouts", (req, res) => {});
+// Creates a workout using data in the request body.
+router.post("/api/workouts", (req, res) => {
 
-router.put("/api/workouts/:id", (req, res) => {});
+  // CODE HERE
+  const {body} = req;
+  db.Workout.create(body)
+  .then(({_id})=>{
+    db.Workout.updateOne({}, {$push: {} })
+  })
+});
 
-router.get("/api/workouts", (req, res) => {});
+// Respond with workout for id url parameter. This should
+// respond with the updated workout json
+router.put("/api/workouts/:id", (req, res) => {
+  // CODE HERE
+  const {params} = req
+});
 
-router.get("/api/workouts/range", (req, res) => {});
+// Respond with json for all the workouts in an array.
+router.get("/api/workouts", (req, res) => {
+  // CODE HERE
+});
 
-router.delete("/api/workouts", (req, res) => {});
+// Respond with json array containing the last 7 workouts
+router.get("/api/workouts/range", (req, res) => {
+  // CODE HERE
+});
+
+// Delete workout with id matching id in the request body.
+router.delete("/api/workouts", (req, res) => {
+  // CODE HERE
+});
 
 module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const logger = require("morgan");
 
-// TODO: import mongoose
+// // TODO: import mongoose
+const mongojs = require("mongojs");
 const mongoose = require("mongoose");
 
 const apiRoutes = require("./routes/api");
@@ -18,17 +19,58 @@ app.use(express.json());
 
 app.use(express.static("public", { "extensions": "html" }));
 
+const Schema = mongoose.Schema;
+
+const workoutSchema = new mongoose.Schema({
+  type:{
+    type: String,
+    trim: true,
+    required: "string is required",
+  },
+  name: {
+    type: String,
+    trim: true,
+    required: "string is required",
+  },
+  duration: {
+    type: Number,
+    required: true,
+    unique: true,
+    min: 1,
+  },
+  weight: {
+    type: Number,
+    required: true,
+    unique: true,
+    min: 1,
+  },
+  reps: {
+    type: Number,
+    required: true,
+    unique: true,
+    min: 1,
+  },
+  sets: {
+    type: Number,
+    required:true,
+    unique: true,
+    min:1,
+  }
+});
+
+const User = mongoose.model("Workout", workoutSchema);
+
+module.exports = User;
+
+
+
+
 // TODO: create mongodb connection with mongoose
-  mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
 });
-
-
-const Workout = mongoose.model("Workout", workoutSchema);
-
-module.exports = Workout;
 
 
 app.use(apiRoutes);
@@ -36,41 +78,3 @@ app.use(apiRoutes);
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
-
-// const workoutSchema = new mongoose.Schema({
-//   type:{
-//     type: String,
-//     trim: true,
-//     required: "string is required",
-//   },
-//   name: {
-//     type: String,
-//     trim: true,
-//     required: "string is required",
-//   },
-//   duration: {
-//     type: Number,
-//     required: true,
-//     unique: true,
-//     min: 1,
-//   },
-//   weight: {
-//     type: Number,
-//     required: true,
-//     unique: true,
-//     min: 1,
-//   },
-//   reps: {
-//     type: Number,
-//     required: true,
-//     unique: true,
-//     min: 1,
-//   },
-//   sets: {
-//     type: Number,
-//     required:true,
-//     unique: true,
-//     min:1,
-//   }
-
-// })
